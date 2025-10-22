b3912e2bb1e586fdabf74bf9530ae4a3cfd645ef
vinaychandran-git <69992040+vinaychandran-git@users.noreply.github.com>
2025-10-22 22:25:04 +0530

    Add files via upload
    Initial upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'quiz-app-cache-v1';
+const urlsToCache = [
+  './quiz.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch Cached Content
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
